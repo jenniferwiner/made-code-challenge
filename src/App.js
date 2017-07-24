a62ce381cafd81f4cc2e6caee628733c3dc12e0b
jenniferwiner <jeniwiner@gmail.com>
2017-07-24 16:00:09 -0600

    fix footer for mobile and desktop

--- src/App.js
+++ src/App.js
@@ -7,39 +7,41 @@ import imgSave from './images/img-save.png'
 class App extends Component {
   render() {
     return (
-      <div className="profile">
-        <div className="header text-center">
-          <div className="pull-left nav">
-            <img src={iconHamburgerNav} alt="navigation"/>
-          </div>
-          <div className="text-center header-text">
-            <h1>Profile</h1>
-          </div>
-        </div>
-        <div className="photo-container text-center">
-          <img src={imgProfile} alt="profile-img"/>
-        </div>
-        <form className="profile-form">
-          <div className="profile-form-content">
-              <label>
-                First name
-                <input type="text" name="firstname" id="firstname" placeholder="Jane Doe"/>
-              </label>
-              <label>
-                Job title
-                <input type="text" name="jobtitle" id="jobtitle" placeholder="Developer"/>
-              </label>
-              <label>
-                Birthday
-                <input type="date" name="birthday" id="birthday"/>
-              </label>
-              <label>
-                Bio
-                <textarea type="text" name="bio" id="bio" placeholder="A few words about yourself"/>
-              </label>
+      <div>
+        <div className="profile">
+          <div className="header text-center">
+            <div className="pull-left nav">
+              <img src={iconHamburgerNav} alt="navigation"/>
             </div>
-            <img src={imgSave} className="save-button pull-right" alt="save-button"/>
-        </form>
+            <div className="text-center header-text">
+              <h1>Profile</h1>
+            </div>
+          </div>
+          <div className="photo-container text-center">
+            <img src={imgProfile} alt="profile-img"/>
+          </div>
+          <form className="profile-form">
+            <div className="profile-form-content">
+                <label>
+                  First name
+                  <input type="text" name="firstname" id="firstname" placeholder="Jane Doe"/>
+                </label>
+                <label>
+                  Job title
+                  <input type="text" name="jobtitle" id="jobtitle" placeholder="Developer"/>
+                </label>
+                <label>
+                  Birthday
+                  <input type="date" name="birthday" id="birthday"/>
+                </label>
+                <label>
+                  Bio
+                  <textarea type="text" name="bio" id="bio" placeholder="A few words about yourself"/>
+                </label>
+              </div>
+              <img src={imgSave} className="save-button pull-right" alt="save-button"/>
+          </form>
+        </div>
         <footer>
         </footer>
       </div>
